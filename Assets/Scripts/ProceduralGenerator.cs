98075c05b8f6900ea173f21ddcbd55028d5d1480
theishiopian <theishiopian@gmail.com>
2018-05-29 17:44:25 -0700

    Uploaded pjoject files

---
+++ Assets/Scripts/ProceduralGenerator.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour
+{
+	public static int size;
+
+	[SerializeField]
+	GameObject cube;
+
+	static String[] roomShapes =
+	{
+		"CR",
+		"Er",
+		"Eu",
+		"El",
+		"Ed",
+		"Ul",
+		"Ur",
+		"Dl",
+		"Dr",
+		"Vr",
+		"Hr"
+	};
+
+	public enum RoomTypes
+	{
+		//11 in total, ya dingus
+		CROSS = 0,
+		END_RIGHT = 1,
+		END_UP = 2,
+		END_LEFT = 3,
+		END_DOWN = 4,
+		UP_LEFT = 5,
+		UP_RIGHT = 6,
+		DOWN_LEFT = 7,
+		DOWN_RIGHT = 8,
+		VERTICAL = 9,
+		HORIZONTAL = 10
+	}
+
+
+
+	public class Room
+	{
+
+
+
+		static System.Random rand = new System.Random();
+
+		public RoomTypes type;
+
+		int x;
+
+		int y;
+
+		public Room(RoomTypes type, int posX, int posY)
+		{
+			this.type = type;
+			this.x = posX;
+			this.y = posY;
+			map[-y + 2, x + 2] = this;
+
+			generateFrom(type);
+		}
+
+		void generateFrom(RoomTypes type)
+		{
+
+
+			switch (type)
+			{
+			//fuck da police
+			case RoomTypes.CROSS:
+				{
+					//top
+					if (getAtCoords(x, y + 1) == null)
+					{
+						spawnUp(x, y);
+					}
+
+					//right
+					if (getAtCoords(x + 1, y) == null)
+					{
+						spawnRight(x, y);
+					}
+
+					//bottom
+					if (getAtCoords(x, y - 1) == null)
+					{
+						spawnDown(x, y);
+					}
+
+					//left
+					if (getAtCoords(x - 1, y) == null)
+					{
+						spawnLeft(x, y);
+					}
+
+				}
+				break;
+			case RoomTypes.UP_LEFT:
+				{
+					if (getAtCoords(x - 1, y) == null)//left
+					{
+						if (x - 1 == -(size / 2))//termination is important
+						{
+							new Room(RoomTypes.END_LEFT, x - 1, y);
+						}
+						else
+						{
+							spawnLeft(x, y);
+						}
+					}
+					if (getAtCoords(x, y - 1) == null)//down
+					{
+						if (y - 1 == -(size / 2))//termination is important
+						{
+							new Room(RoomTypes.END_UP, x, y - 1);
+						}
+						else
+						{
+							spawnDown(x, y);
+						}
+					}
+				}
+				break;
+			case RoomTypes.UP_RIGHT:
+				{
+					if (getAtCoords(x + 1, y) == null)//right
+					{
+						if (x + 1 == size / 2)//termination is important
+						{
+							new Room(RoomTypes.END_RIGHT, x + 1, y);
+						}
+						else
+						{
+							spawnRight(x, y);
+						}
+					}
+					if (getAtCoords(x, y - 1) == null)//down
+					{
+						if (y - 1 == -(size / 2))//termination is important
+						{
+							new Room(RoomTypes.END_DOWN, x, y - 1);
+						}
+						else
+						{
+							spawnDown(x, y);
+						}
+					}
+				}
+				break;
+			case RoomTypes.DOWN_LEFT:
+				{
+					if (getAtCoords(x - 1, y) == null)//left
+					{
+						if (x - 1 == -(size / 2))//termination is important
+						{
+							new Room(RoomTypes.END_LEFT, x - 1, y);
+						}
+						else
+						{
+							spawnLeft(x, y);
+						}
+					}
+					if (getAtCoords(x, y + 1) == null)//up
+					{
+						if (y + 1 == size / 2)//termination is important
+						{
+							new Room(RoomTypes.END_UP, x, y + 1);
+						}
+						else
+						{
+							spawnUp(x, y);
+						}
+					}
+				}
+				break;
+			case RoomTypes.DOWN_RIGHT:
+				{
+					if (getAtCoords(x + 1, y) == null)//right
+					{
+						if (x + 1 == size / 2)//termination is important
+						{
+							new Room(RoomTypes.END_RIGHT, x + 1, y);
+						}
+						else
+						{
+							spawnRight(x, y);
+						}
+					}
+					if (getAtCoords(x, y + 1) == null)//up
+					{
+						if (y + 1 == size / 2)//termination is important
+						{
+							new Room(RoomTypes.END_UP, x, y + 1);
+						}
+						else
+						{
+							spawnUp(x, y);
+						}
+					}
+				}
+				break;
+			case RoomTypes.VERTICAL:
+				{
+					if (getAtCoords(x, y + 1) == null)//up
+					{
+						if (y + 1 == size / 2)//termination is important
+						{
+							new Room(RoomTypes.END_UP, x, y + 1);
+						}
+						else
+						{
+							spawnUp(x, y);
+						}
+					}
+					if (getAtCoords(x, y - 1) == null)//down
+					{
+						if (y - 1 == -(size / 2))//termination is important
+						{
+							new Room(RoomTypes.END_DOWN, x, y - 1);
+						}
+						else
+						{
+							spawnDown(x, y);
+						}
+					}
+				}
+				break;
+			case RoomTypes.HORIZONTAL:
+				{
+					if (getAtCoords(x + 1, y) == null)//right
+					{
+						if (x + 1 == size / 2)//termination is important
+						{
+							new Room(RoomTypes.END_RIGHT, x + 1, y);
+						}
+						else
+						{
+							spawnRight(x, y);
+						}
+					}
+					if (getAtCoords(x - 1, y) == null)//left
+					{
+						if (x - 1 == -(size / 2))//termination is important
+						{
+							new Room(RoomTypes.END_LEFT, x - 1, y);
+						}
+						else
+						{
+							spawnLeft(x, y);
+						}
+					}
+				}
+				break;
+			}
+		}
+
+		static void spawnUp(int x, int y)
+		{
+			//1 to 4
+			int g = rand.Next(4) + 1;
+
+			switch (g)
+			{
+			case 1:
+				new Room(RoomTypes.VERTICAL, x, y + 1);
+				break;
+			case 2:
+				new Room(RoomTypes.UP_LEFT, x, y + 1);
+				break;
+			case 3:
+				new Room(RoomTypes.UP_RIGHT, x, y + 1);
+				break;
+			case 4:
+				new Room(RoomTypes.END_UP, x, y + 1);
+				break;
+			}
+		}
+
+		static void spawnDown(int x, int y)
+		{
+			//1 to 4
+			int g = rand.Next(4) + 1;
+
+			switch (g)
+			{
+			case 1:
+				new Room(RoomTypes.VERTICAL, x, y - 1);
+				break;
+			case 2:
+				new Room(RoomTypes.DOWN_LEFT, x, y - 1);
+				break;
+			case 3:
+				new Room(RoomTypes.DOWN_RIGHT, x, y - 1);
+				break;
+			case 4:
+				new Room(RoomTypes.END_DOWN, x, y - 1);
+				break;
+			}
+		}
+
+		static void spawnLeft(int x, int y)
+		{
+			//1 to 4
+			int g = rand.Next(4) + 1;
+
+			switch (g)
+			{
+			case 1:
+				new Room(RoomTypes.HORIZONTAL, x - 1, y);
+				break;
+			case 2:
+				new Room(RoomTypes.UP_RIGHT, x - 1, y);
+				break;
+			case 3:
+				new Room(RoomTypes.DOWN_RIGHT, x - 1, y);
+				break;
+			case 4:
+				new Room(RoomTypes.END_LEFT, x - 1, y);
+				break;
+			}
+		}
+
+		static void spawnRight(int x, int y)
+		{
+			//1 to 4
+			int g = rand.Next(4) + 1;
+
+			switch (g)
+			{
+			case 1:
+				new Room(RoomTypes.HORIZONTAL, x + 1, y);
+				break;
+			case 2:
+				new Room(RoomTypes.DOWN_LEFT, x + 1, y);
+				break;
+			case 3:
+				new Room(RoomTypes.UP_LEFT, x + 1, y);
+				break;
+			case 4:
+				new Room(RoomTypes.END_RIGHT, x + 1, y);
+				break;
+			}
+		}
+
+		public override String ToString()
+		{
+			if (this == null)return "  ";
+			return roomShapes[(int)type];
+		}
+	}
+
+	public static Room[,] map;
+
+	public static Room getAtCoords(int x, int y)
+	{
+
+		Room room = map[-y + 2, x + 2];
+
+		return room;
+	}
+
+	void Start()
+	{
+		size = 5;
+
+		map = new Room[5, 5];
+
+		Vector3 pos = new Vector3 ();
+
+		Room start = new Room(RoomTypes.CROSS, 0, 0);
+
+		for (int y = 2; y >= -2; y--)
+		{
+			for (int x = -2; x <= 2; x++)
+			{
+				pos.Set((float)x,((float)y),0.0f);
+				if (getAtCoords(x, y) != null)//no, YOU'RE null, bitch!
+				{
+					Instantiate(cube, pos, Quaternion.identity);
+				}
+			}
+		}
+	}
+}
